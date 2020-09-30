6272fdff7a9c8f32355b6032fc7016e4beee16f7
ACM Safrin <mylifeofjava@gmail.com>
2020-09-30 13:06:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get("/",function(req,res){
   res.send("Hello")
 })
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
+}
+
 
 let connectionString = 'mongodb+srv://todoappuser:todoappuser@cluster0.7fin5.mongodb.net/complex_app?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
