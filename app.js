a64b5843d40ddca6575f81ba49f1049879422f92
ACM Safrin <mylifeofjava@gmail.com>
2020-09-30 12:53:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express')
+let mongodb = require('mongodb')
 
 const app=express()
 
@@ -6,4 +7,10 @@ app.get("/",function(req,res){
   res.send("Hello")
 })
 
-app.listen(process.env.PORT||3000)
+
+let connectionString = 'mongodb+srv://todoappuser:todoappuser@cluster0.7fin5.mongodb.net/complex_app?retryWrites=true&w=majority'
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  console.log(err)
+  db = client.db()
+  app.listen(port)
+})
